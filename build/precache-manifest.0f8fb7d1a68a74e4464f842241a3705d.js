7990e976e6eb744a0043b49868ae263c5bfb3676
mhezzet <40916821+mhezzet@users.noreply.github.com>
2020-02-15 17:33:32 +0200

    build project and added readme

---
+++ build/precache-manifest.0f8fb7d1a68a74e4464f842241a3705d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7dd7fb82d6565d746b96c6e9077d105",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b841cd20e0dda7d4c064",
+    "url": "/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "bdbbad8f2687bb990af6",
+    "url": "/static/css/main.395f9bb5.chunk.css"
+  },
+  {
+    "revision": "b841cd20e0dda7d4c064",
+    "url": "/static/js/2.86e0ff54.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.86e0ff54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdbbad8f2687bb990af6",
+    "url": "/static/js/main.6018048d.chunk.js"
+  },
+  {
+    "revision": "79753322de31b2fb4c07",
+    "url": "/static/js/runtime-main.f8c72acc.js"
+  }
+]);
